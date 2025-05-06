74628ac49c028aaf72bed743ff2111d84b7da984
xerneas02 <71772411+xerneas02@users.noreply.github.com>
2025-05-07 00:30:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-export LOGIN_INSTAGRAM="imageduglobe"
-export PASSWORD_INSTAGRAM="wSW@x3PDhzGb#8^q"
-export OPENW_MAP_KEY="a7422ccdb985fee8986b950821a65f9e"
-export GOOGLE_KEY="AIzaSyCy4hb3E5eaX0bv1Qd46lQ5htSl85EaISs"
 
-python src/botInsta.py
